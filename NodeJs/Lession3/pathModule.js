26f2b9636c7025bbd8e463832aa69ba0035eb2af
Md Nazmul Hasan <nazmul.ns7989@gmail.com>
2022-09-05 21:55:26 +0600

    practicing os and path module

---
+++ NodeJs/Lession3/pathModule.js
@@ -0,0 +1,8 @@
+const path = require('path');
+
+// console.log(__dirname);
+// console.log(__filename)
+
+// console.log(path.extname('index.html'))
+// console.log(path.join(__dirname + '/newfoder'))
+// console.log(path.join(__dirname + '/../newfoder'))
