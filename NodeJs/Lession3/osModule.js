aab7b65f4773d9c4a33c25b07b58790fdff6ec08
Md Nazmul Hasan <nazmul.ns7989@gmail.com>
2022-09-05 22:14:44 +0600

    os and path module file added

---
+++ NodeJs/Lession3/osModule.js
@@ -0,0 +1,13 @@
+const os = require('os');
+
+// console.log(os.homedir());
+// console.log(os.tmpdir());
+// console.log(os.userInfo());
+// console.log(os.hostname());
+// console.log(os.totalmem());
+// console.log(os.freemem());
+// console.log(os.version());
+// console.log(os.release());
+// console.log(os.type());
+// console.log(os.cpus());
+// console.log(os.loadavg());
