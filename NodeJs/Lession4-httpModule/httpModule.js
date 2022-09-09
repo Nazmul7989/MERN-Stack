e981081cf62c6339f768df744d258b474af2ffa3
Md Nazmul Hasan <72700963+Nazmul7989@users.noreply.github.com>
2022-09-09 22:38:58 +0600

    Added localhost log

--- NodeJs/Lession4-httpModule/httpModule.js
+++ NodeJs/Lession4-httpModule/httpModule.js
@@ -4,8 +4,13 @@ const http = require('http');
 //     res.end()
 // }).listen(8000)
 
+let host = 'localhost';
+let port = 8000;
+
 const server = http.createServer((req,res)=>{
     res.end('Your Server is running in localhost:8000')
 })
 
-server.listen(8000);
+server.listen(8000,(host,port)={
+ console.log(`Server is running successfully at http://${host}:${port}`)
+});
