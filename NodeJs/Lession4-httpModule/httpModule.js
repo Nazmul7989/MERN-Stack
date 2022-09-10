afa100b6eb905dd5673692fdfcd863c9c4375b1b
Md Nazmul Hasan <nazmul.ns7989@gmail.com>
2022-09-10 08:09:42 +0600

    add status code in response head

--- NodeJs/Lession4-httpModule/httpModule.js
+++ NodeJs/Lession4-httpModule/httpModule.js
@@ -1,16 +1,28 @@
 const http = require('http');
+const host = '127.0.0.1';
+const port = 8000;
 
 // http.createServer((req,res)=>{
 //     res.end()
 // }).listen(8000)
 
-let host = 'localhost';
-let port = 8000;
+//
+// const server = http.createServer((req,res)=>{
+//     res.end("<h3>Your Server is running in localhost:8000</h3>")
+// })
+//
+// server.listen(port,host, ()=>{
+//  console.log(`Server is running successfully at http://${host}:${port}`)
+// });
+
+
 
 const server = http.createServer((req,res)=>{
-    res.end('Your Server is running in localhost:8000')
+    res.writeHead(200,{'Content-Type': 'text/html'})
+    res.write("<h3>Your Server is running in localhost:8000</h3>")
+    res.end()
 })
 
-server.listen(8000,(host,port)={
- console.log(`Server is running successfully at http://${host}:${port}`)
+server.listen(port,host, ()=>{
+    console.log(`Server is running successfully at http://${host}:${port}`)
 });
