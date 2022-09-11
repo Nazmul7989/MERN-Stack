5218635d9c4a0986e713b3c100312a05f8352b30
Md Nazmul Hasan <nazmul.ns7989@gmail.com>
2022-09-11 22:09:10 +0600

    practicing external module

---
+++ NodeJs/Lession5-externalModule/externalModule.js
@@ -0,0 +1,6 @@
+// const getRandomFruitsName = require('random-fruits-name')
+// console.log(getRandomFruitsName())
+const randomMovieNames = require('random-movie-names');
+
+console.log(randomMovieNames());
+
