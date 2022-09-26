1139539117db4d8396c89224f4fe109994819a90
Md Nazmul Hasan <nazmul.ns7989@gmail.com>
2022-09-26 22:18:32 +0600

    practice express response

---
+++ ExpressJs/Lession4-Response/index.js
@@ -0,0 +1,14 @@
+const app = require('./app')
+const port = 8000;
+const host = 'localhost'
+
+
+app.use((req, res) => {
+    res.send('404! Not Found.')
+    res.end();
+})
+
+
+app.listen(port,host,()=>{
+    console.log(`Server is running at http://${host}:${port}`)
+})
