36b842be1316a8d94ce57342a1d81653fd99e9a4
Md Nazmul Hasan <nazmul.ns7989@gmail.com>
2024-11-06 09:11:42 +0600

    practice nodejs fs module

---
+++ practice/nodejs/fs-module/index.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+
+// console.log(fs)
+
+// fs.writeFile('demo.txt','This is demo text',(err)=> {
+//     if (err){
+//         console.log(err)
+//     }else {
+//         console.log('File created successful')
+//     }
+// })
+
+// fs.appendFile('demo.txt',' Another text updated',(err)=> {
+//     if (err){
+//         console.log(err)
+//     }else {
+//         console.log('File updated successful')
+//     }
+// })
+
+// fs.readFile('demo.txt','utf-8',(err,data)=> {
+//     if (err){
+//         console.log(err)
+//     }else {
+//         console.log(data)
+//     }
+// })
+
+// fs.rename('demo.txt','demo1.txt',(err)=> {
+//     if (err){
+//         console.log(err)
+//     }else {
+//         console.log('File renamed successfully')
+//     }
+// })
+
+// fs.exists('demo.txt',(result)=> {
+//     if (result){
+//         console.log('File found')
+//     }else {
+//         console.log('File not found')
+//     }
+// })
+
+// fs.unlink('demo.txt',(err)=> {
+//     if (err){
+//         console.log(err)
+//     }else {
+//         console.log('File removed successfully')
+//     }
+// })
+
+// try {
+//     fs.unlinkSync('demo.txt')
+//     console.log('File removed successfully')
+// }catch (err){
+//     console.log(err)
+// }
+
