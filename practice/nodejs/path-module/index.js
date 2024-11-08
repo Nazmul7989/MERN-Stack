c1fb4ae7d3ac633020ca34d33bf47893ec50a32b
Md Nazmul Hasan <nazmul.ns7989@gmail.com>
2024-11-08 10:23:15 +0600

    practice nodejs path module

---
+++ practice/nodejs/path-module/index.js
@@ -0,0 +1,6 @@
+const path = require('path');
+const file = __filename;
+console.log(path.dirname(file));
+console.log(path.basename(file));
+console.log(path.extname(file));
+console.log(path.join(__dirname + '/views'));
